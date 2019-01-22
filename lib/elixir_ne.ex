4e0ecab82addc4f406b90385268efd126a566401
Dmitry Ledentsov <dmlled@yahoo.com>
2019-01-22 21:14:54 +0100

    mix init

---
+++ lib/elixir_ne.ex
@@ -0,0 +1,18 @@
+defmodule ElixirNe do
+  @moduledoc """
+  Documentation for ElixirNe.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirNe.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_ne_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirNeTest do
+  use ExUnit.Case
+  doctest ElixirNe
+
+  test "greets the world" do
+    assert ElixirNe.hello() == :world
+  end
+end
