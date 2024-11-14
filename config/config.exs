3826276961e7891c9f0b66db91ebabd692039611
Dmitry Ledentsov <dmlled@yahoo.com>
2024-11-14 18:25:00 +0100

    mix format

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,7 @@
 import Config
 
-config :elixir_ne, :number_of_neurons, (System.get_env("N_NEURONS") || "1000") |> String.to_integer()
+config :elixir_ne,
+       :number_of_neurons,
+       (System.get_env("N_NEURONS") || "1000") |> String.to_integer()
 
 # import_config "#{config_env()}.exs"
